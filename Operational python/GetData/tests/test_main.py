5771d06628cfa25900efec429cddd359d513d8e4
purplecat7 <j.p.lewis@reading.ac.uk>
2016-08-31 22:14:08 +0100

    more comprehensive unit tests

--- Operational python/GetData/tests/test_main.py
+++ Operational python/GetData/tests/test_main.py
@@ -17,6 +17,8 @@ import glob
 import nose.tools as ns
 
 from GetData.src.getData import main, create_parser
+from modis_config.src.configuration import Configuration
+from modis_config.src.constants import defs as constants
 
 
 # ensure the file system is clear of test and intermediate files/folders
@@ -28,11 +30,53 @@ def test_clean_up():
             os.remove(fl)
         for fl in glob.glob('.\save*'):
             os.remove(fl)
+        # TODO also need to remove files in user home directory
         os.remove('.\scrape.txt')
     except:
         pass
 
-#  Test many of the command line options and their combinations.
+# Unit test some of the more complex methods
+def set_up_config(mode, the_file):
+    config = Configuration()
+    config.read_config(mode, the_file)
+    return config
+
+def test_default_raw_folder():
+    cfg = set_up_config(0, constants['file'])   # download mode
+    # default dir is ~/data/[tile]/[product]
+    # which will expand to [user]/data/h18v04/MOD09GA
+    expected_folder = os.path.expanduser('~') + '\data\h18v04\MOD09GA' + os.path.sep
+    ns.assert_equals(cfg.get_rawdata_dir(), expected_folder)
+
+def test_dwnld_section_raw_folder():
+    cfg = set_up_config(0, 'land_cover_config_test_download_locPosix.ini')
+    expected_folder = './data1\h18v04\MOD09GA' + os.path.sep
+    ns.assert_equals(cfg.get_rawdata_dir(), expected_folder)
+
+def test_dflt_section_raw_folder():
+    cfg = set_up_config(0, 'land_cover_config_test_download_locWin.ini')
+    expected_folder = '.\data2\h18v04\MOD09GA' + os.path.sep
+    ns.assert_equals(cfg.get_rawdata_dir(), expected_folder)
+
+def test_local_filenames():
+    cfg = set_up_config(0, constants['file'])   # download mode
+    expected_folder = os.path.expanduser('~') + '\data\h18v04\MOD09GA' + os.path.sep
+    expected_filename1 = expected_folder + 'MOD09GA.A2007360.h18v04.005.hdf'
+    expected_filename2 = expected_folder + 'MOD09GA.A2007360.h18v04.005.hdf.xml'
+    files = cfg.create_local_filenames()
+    ns.assert_equals(files, [expected_filename1, expected_filename2])
+
+def test_web_page_addr():
+    cfg = set_up_config(0, constants['file'])   # download mode
+    # products starting with 'MOD' require the 'MOLT' download area
+    expected_web_addr = 'http://e4ftl01.cr.usgs.gov/MOLT/MOD09GA.005/2007.12.26'
+    web_addr = cfg.create_URL()
+    ns.assert_equals(web_addr, expected_web_addr)
+
+
+
+# Test the configuration options and their combinations.
+# setting 'testing_args' prevents any web access
 def test_success_default_ini_file():
     args = create_parser([])
     ns.assert_true(main(args, testing_args=True))
@@ -48,8 +92,10 @@ def test_file_arg_file_missing():
     main(args, testing_args=True)
 
 
-#  Test a download, use the default config mostly as this is what's been set up in the page.
-#  However, there is only a link for day 364, so need to specify start & end as day 364.
+# Test a download, use the default config mostly as this is what's been set up in the page.
+# setting test==Git uses the dummy website to scrape and dummy files to download.
+# setting test==Real uses real website to scrape and real files but only the first 1000 bytes.
+# TODO more detailed tests
 def test_file_download():
     args = create_parser(['-file', 'land_cover_config_test_download.ini', '-test', 'Git'])
     ns.assert_false(main(args))
